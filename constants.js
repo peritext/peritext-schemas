32bf5b7f88f157e70f516a4a86efe83516c93e94
Robin de Mourat <robin.demourat@gmail.com>
2019-01-12 16:27:16 +0100

    add schemas

---
+++ constants.js
@@ -0,0 +1,8 @@
+module.exports = {
+  draftEntitiesNames: {
+    LINK: 'LINK',
+    BLOCK_ASSET: 'BLOCK_ASSET',
+    INLINE_ASSET: 'INLINE_ASSET',
+    SECTION_POINTER: 'SECTION_POINTER',
+  }
+};
---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  constants: require( './constants' ),
+  production: require( './production' ),
+  resource: require( './resource' ),
+  contextualizer: require( './contextualizer' ),
+};
